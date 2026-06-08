dc01d22c89822bf3fd928d3bc8ef8eb83ef27842
tingyu <tingyuz483@gmail.com>
2026-06-08 16:32:51 +0800

    Initial Commit

---
+++ final_project/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  final_project
+//
+//  Created by tingyu  on 2026/6/8.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
